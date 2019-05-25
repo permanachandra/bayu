2750daffdd3e1e789dc226c37b43333ad53bd540
Bayu Chandra <permanachandrabayu@gmail.com>
2019-05-25 23:13:10 +0700

    list box

--- js/script.js
+++ js/script.js
@@ -12,18 +12,9 @@ $.ajax({
         $.each(surat, function(i, data){
             //console.log(data.englishName);
 
-            $('#list-surat').append (`
-           
-            <div class="dropdown">
-        
-                <a class="dropdown-item" href="#">${data.englishName}</a>
-             
-            </div> 
-            
-                
-            
-            
-  
+            $('#list-surat').append (`   
+
+              <option>${data.englishName}</option>    
              
            `)
 
